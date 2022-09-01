2f8bce6f4a8b6e134ee77de89a55eeb9a6478d34
PavelStadnuk <bob31021@gmail.com>
2022-09-01 18:54:41 +0200

    ffdd

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import MeinApp from './App'
-test('renders learn react link', () => {
-	render(<MeinApp />)
-	/* const linkElement = screen.getByText(/learn react/i) */
-	/* expect(linkElement).toBeInTheDocument() */
-})
